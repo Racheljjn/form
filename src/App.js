b5de90eef49abb29c963eb4cdcdebf90d1c58ba7
Racheljjn <jian1160@mylaurier.ca>
2020-12-16 17:17:07 -0500

    updated version one

--- src/App.js
+++ src/App.js
@@ -1,140 +1,313 @@
-import React, { Component } from 'react';
-import './App.css'
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import NumberFormat from "react-number-format";
 
+let current_date = "2020-11-26";
 
-
-let currentDate = "2020-11-26"
 class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      outdated:[],
-      date:"",
-    }
-  }
-  
-  handleDateChange=(e)=>{
-    e.preventDefault()
-    this.setState({date: e.target.value }) 
-
+  constructor() {
+    super();
+    this.state = {
+      selected_fund_ID: "IGB",
+      date: "",
+      outdated_fund_data: {},
+      original_fund_data: {},
+      input_data: {},
+    };
   }
 
-  handleAUMChange=(e)=>{
-    e.preventDefault()
+  handleFundSelection = (e) => {
+    e.preventDefault();
+    this.setState({ selected_fund_ID: e.target.value });
+  };
+
+  handleDateChange = (e) => {
+    e.preventDefault();
+    const outdated_fund_data = { ...this.state.outdated_fund_data };
+    const input_data = { ...this.state.input_data };
+    input_data["date"] = e.target.value;
+    // Object.keys(outdated_fund_data).map((key) => {
+    //   return (outdated_fund_data[key].date = e.target.value);
+    // });
+    this.setState({ date: e.target.value, outdated_fund_data, input_data });
+  };
+
+  handleAUMChange = (e, name, fundID) => {
+    e.preventDefault();
+    const outdated_fund_data = { ...this.state.outdated_fund_data };
+    const input_data = { ...this.state.input_data };
+
+    outdated_fund_data[fundID].aum = Number(
+      e.target.value.replace(/[^0-9\.]+/g, "")
+    );
+    input_data[fundID].aum = Number(e.target.value.replace(/[^0-9\.]+/g, ""));
+
     this.setState({
-      [e.target.name]:Number(e.target.value.trim())
-    })
-
-  }
-  handleNAVChange=(e)=>{
-  e.preventDefault()
+      [name]: e.target.value,
+      outdated_fund_data,
+      input_data,
+    });
+    console.log(this.state.input_data);
+  };
+  handleNAVChange = (e, seriesID, selected_fund_ID) => {
+    e.preventDefault();
+    const outdated_fund_data = { ...this.state.outdated_fund_data };
+    const input_data = { ...this.state.input_data };
+    outdated_fund_data[selected_fund_ID].series[seriesID] = Number(
+      e.target.value.replace(/[^0-9\.]+/g, "")
+    );
+    input_data[selected_fund_ID].series[seriesID] = Number(
+      e.target.value.replace(/[^0-9\.]+/g, "")
+    );
     this.setState({
-      [e.target.name]:Number(e.target.value.trim())
-    })
-  }
+      [e.target.name]: e.target.value,
+      outdated_fund_data,
+      input_data,
+    });
+    console.log(this.state.input_data);
+  };
 
-  submitHandler=(e)=>{
-    e.preventDefault()
-    let newObj = {...this.state}
-    delete newObj.outdated;
+  submitHandler = (e) => {
+    e.preventDefault();
+    let newObj = { ...this.state.input_data };
+
     // newObj = { date: "2020-11-27", IGB: "30", IGB_A: "20", IGB_B: "10", MJJ: "60", MJJ_A: "30", MJJ_F: "20" }
-    axios.post(`http://localhost/server/demo.php`, JSON.stringify(newObj))
-    .then(res=>console.log(res))
-    .catch(err=>{
-      console.log(err)
-    })
+    axios
+      .post(`http://localhost/server/demo.php`, JSON.stringify(newObj))
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err);
+      });
 
-    this.successMsg()
-
-  }
+    this.successMsg();
+  };
 
   // display a success msg once form's been submitted
-  successMsg=()=>{
-    alert("form submitted successfully")
+  successMsg = () => {
+    alert("form submitted successfully");
+  };
 
+  /***************************
+   fetch data and organize data
+   ****************************/
+  componentDidMount() {
+    this.fetchData();
   }
+  fetchData = async () => {
+    try {
+      const result = await fetch(
+        `https://purposecloud.s3.amazonaws.com/challenge-data.json`
+      );
+      const data = await result.json();
 
+      const simplified_fund_data = {};
 
+      for (let prop in data) {
+        if (data.hasOwnProperty(prop)) {
+          simplified_fund_data[prop] = {
+            aum: data[prop].aum,
+            name: data[prop].name.en,
+            series: data[prop].series,
+            date: "",
+          };
+          for (let key in data[prop].series) {
+            if (data[prop].series.hasOwnProperty(key)) {
+              simplified_fund_data[prop].date =
+                data[prop].series[key].latest_nav.date;
+              simplified_fund_data[prop].series[key] =
+                data[prop].series[key].latest_nav.value;
+            }
+          }
+        }
+      }
 
-
-// fetch data from json file
-  componentDidMount(){
-    this.fetchData()
-  
-  }
-
-
-  fetchData = async() =>{
-   
-  try{
-
-     const result = await fetch(`https://purposecloud.s3.amazonaws.com/challenge-data.json`)
-      const data = await result.json()
-      // refactoring data
-      const allArray = Object.entries(data)
-  
-      const newAllArray = allArray.map(item =>{
-        
-        return [item[0], item[1].name, item[1].series,  item[1].aum]
-      })
-
-      let outdated = newAllArray.filter(item =>{
-        return Object.entries(item[2]).every(piece=> {return new Date(piece[1].latest_nav.date).getTime() < new Date(currentDate).getTime() ? piece : null})
-
-      } ).filter(item => item.length > 0)
-
-      outdated = outdated.map(fund => {
-       return [fund[0], Object.keys(fund[2]),fund[1].en ]
-      })
-
-      this.setState({outdated})
-      
-   
-
-  }catch(err){
-    console.log(err)
-  }
-
-
-  }
-
-  // fetch data ends
+      /**********************************
+      filter out funds which are outdated
+      ************************************/
+      for (let id in simplified_fund_data) {
+        if (simplified_fund_data.hasOwnProperty(id)) {
+          if (
+            new Date(simplified_fund_data[id].date).getTime() >
+            new Date(current_date).getTime()
+          ) {
+            delete simplified_fund_data[id];
+          }
+        }
+      }
+      console.log(simplified_fund_data);
+      let outdated_fund_data = JSON.parse(JSON.stringify(simplified_fund_data));
+      let input_data = JSON.parse(JSON.stringify(simplified_fund_data));
+      Object.keys(input_data).map((key) => {
+        return (input_data[key] = {
+          series: {},
+          name: outdated_fund_data[key].name,
+          aum: undefined,
+        });
+      });
+      console.log(input_data);
+      this.setState({ outdated_fund_data });
+      this.setState({ input_data });
+      this.setState({ original_fund_data: simplified_fund_data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   render() {
-    const {outdated} = this.state
+    const {
+      outdated_fund_data,
+      original_fund_data,
+      selected_fund_ID,
+      date,
+    } = this.state;
+
     return (
       <div>
-      <h1>Outdated Funds</h1>
-      <form onSubmit={this.submitHandler}>
-        <label htmlFor="date" aria-label="date">Date: </label>
-        <input type="date" min={currentDate} id="date" name="date" onChange={this.handleDateChange} value={this.state.date} placeholder="type new date" required/>
-        <button type="submit">submit</button>
-        {
-          outdated.map((item,index) =>{
-            return <div>
-                  <h2>{`${item[2]}-${item[0]}`}</h2>
-                  <label htmlFor="AUM">AUM: </label>
-                  <input type="number" step="any" id="AUM" name={item[0]} onChange={this.handleAUMChange}  value={this.state.index} placeholder="type new aum" required/>
-                  
-                   <ul>
-                    
-                    {item[1].map((n,key) => {
-                    return <div>
-                       <li className="series" key={key}>SeriesID: {n}</li>
-                       <label htmlFor="NAV">NAV: </label>
-                       <input type="number" step="any" id="NAV" value={this.state.key} onChange={this.handleNAVChange} name={`${item[0]}__${n}`} placeholder="type new nav" required />
-                    </div>})}                   
-                    </ul>                  
+        <h1>out-of-date data</h1>
+
+        <form id="updated_form" onSubmit={this.submitHandler}>
+          <main className="date_fund_AUM">
+            <section className="date">
+              <div className="date_picker">
+                <label htmlFor="date" aria-label="date">
+                  <span className="date_label">Date:</span>{" "}
+                </label>
+                <input
+                  type="date"
+                  min={current_date}
+                  id="date"
+                  name="date"
+                  onChange={this.handleDateChange}
+                  value={date}
+                />
+                <p className="outdated_date">
+                  {original_fund_data[selected_fund_ID]
+                    ? original_fund_data[selected_fund_ID].date
+                    : null}
+                </p>
               </div>
-          })
-        }
-      </form>
+              <p className="instructions">
+                (please note* the red numbers are stale numbers)
+              </p>
+            </section>
+
+            <section className="fund_AUM">
+              <div className="fund">
+                <label htmlFor="selection">Choose a fund:</label>
+                <select
+                  id="selection"
+                  value={selected_fund_ID}
+                  onChange={this.handleFundSelection}
+                >
+                  {Object.keys(outdated_fund_data).map((key, index) => {
+                    return (
+                      <option className="option" key={index} value={key}>
+                        {outdated_fund_data[key].name}({key})
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              <div className="AUM_data">
+                {outdated_fund_data[selected_fund_ID]
+                  ? Object.keys(outdated_fund_data).map((fundID) => {
+                      if (fundID === selected_fund_ID) {
+                        return (
+                          <div className="AUM_label">
+                            <label htmlFor="AUM">AUM: </label>
+
+                            <NumberFormat
+                              id="AUM"
+                              thousandSeparator={true}
+                              prefix={"$"}
+                              displayType="number"
+                              name={outdated_fund_data[selected_fund_ID].name}
+                              value={
+                                this.state[
+                                  outdated_fund_data[selected_fund_ID].name
+                                ]
+                              }
+                              onChange={(e) => {
+                                this.handleAUMChange(
+                                  e,
+                                  outdated_fund_data[selected_fund_ID].name,
+                                  fundID
+                                );
+                              }}
+                            />
+                            <p className="outdated_AUM">
+                              {original_fund_data[fundID]
+                                ? original_fund_data[fundID].aum.toLocaleString(
+                                    "en-US",
+                                    {
+                                      style: "currency",
+                                      currency: "USD",
+                                    }
+                                  )
+                                : null}
+                            </p>
+                          </div>
+                        );
+                      }
+                    })
+                  : null}
+              </div>
+            </section>
+          </main>
+
+          <ul>
+            {outdated_fund_data[selected_fund_ID]
+              ? Object.keys(outdated_fund_data).map((fundID) => {
+                  if (fundID === selected_fund_ID) {
+                    return Object.keys(outdated_fund_data[fundID].series).map(
+                      (seriesID, index) => {
+                        return (
+                          <div className="series">
+                            <li className="seriesID" key={index}>
+                              Series: {seriesID}
+                            </li>
+                            <NumberFormat
+                              className="nav_input"
+                              thousandSeparator={true}
+                              prefix={"$"}
+                              displayType="number"
+                              name={`${fundID}_${seriesID}`}
+                              value={this.state.index}
+                              onChange={(e) => {
+                                this.handleNAVChange(
+                                  e,
+                                  seriesID,
+                                  selected_fund_ID
+                                );
+                              }}
+                            />
+                            <li className="outdated_NAV">
+                              {" "}
+                              $
+                              {original_fund_data[fundID]
+                                ? original_fund_data[fundID].series[seriesID]
+                                : null}
+                            </li>
+                          </div>
+                        );
+                      }
+                    );
+                  }
+                })
+              : null}
+          </ul>
+
+          <div className="btn">
+            <button type="submit" id="submit">
+              submit
+            </button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
-
